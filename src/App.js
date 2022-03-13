1886de950b1b74d07373f6365aa786e2e9f5e6fd
aylancOnur <aylanc.onur@gmail.com>
2022-03-13 16:03:59 +0300

    pages added and react-router configured

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Favorites from "./pages/Favorites";
+import Orders from "./pages/Orders";
+import Products from "./pages/Products";
+import Product from "./pages/Product";
+import UserProfile from "./pages/UserProfile";
+import Login from "./pages/Login";
+
 const App = () => {
+  const user = true;
   return (
-    <div class="bg-slate-100 text-tahiti">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/products/:category" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
